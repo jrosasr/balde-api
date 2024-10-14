f04f708f7200931c00763fa996814bfd2bcf352e
Juan Rosas <juan.rosasr01@gmail.com>
2024-10-13 22:31:36 -0400

    add route testing

--- routes/auth.php
+++ routes/auth.php
@@ -8,6 +8,10 @@ use App\Http\Controllers\Auth\RegisteredUserController;
 use App\Http\Controllers\Auth\VerifyEmailController;
 use Illuminate\Support\Facades\Route;
 
+Route::get('/testing', function () {
+    return 'Hello World';
+});
+
 Route::post('/register', [RegisteredUserController::class, 'store'])
     ->middleware('guest')
     ->name('register');
